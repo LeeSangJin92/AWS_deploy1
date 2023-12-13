d39b740d9e126f0d54614e0c62418926380ce804
LeeSangJin92 <yomko65@gmail.com>
2023-12-13 09:37:58 +0900

    Initial commit

---
+++ src/test/java/com/example/aws_deploy1/AwsDeploy1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.aws_deploy1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsDeploy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
